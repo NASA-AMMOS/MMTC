e1fd1eed7a494b379b1e93966aae0ed21246fb25
John Lage <john.lage@jhuapl.edu>
2025-09-17 16:20:39 +0000

    mmtc-gh-8 clean up imports

--- mmtc-core/src/main/java/edu/jhuapl/sd/sig/mmtc/products/definition/SclkKernelProductDefinition.java
+++ mmtc-core/src/main/java/edu/jhuapl/sd/sig/mmtc/products/definition/SclkKernelProductDefinition.java
@@ -6,10 +6,7 @@ import edu.jhuapl.sd.sig.mmtc.correlation.TimeCorrelationContext;
 import edu.jhuapl.sd.sig.mmtc.products.definition.util.ProductWriteResult;
 import edu.jhuapl.sd.sig.mmtc.products.definition.util.ResolvedProductDirPrefixSuffix;
 import edu.jhuapl.sd.sig.mmtc.products.model.SclkKernel;
-import edu.jhuapl.sd.sig.mmtc.products.model.TextProductException;
-import edu.jhuapl.sd.sig.mmtc.util.TimeConvertException;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
@@ -49,7 +46,7 @@ public class SclkKernelProductDefinition extends EntireFileOutputProductDefiniti
     }
 
     /**
-     * This implementation is notable from that of other output products in that it does still write the SCLK kernel to
+     * This implementation is distinct from that of other output products in that it does still write the SCLK kernel to
      * disk. The only difference is that it's written to the /tmp directory and its latest two lines are recorded here.
      * @param ctx The active run's TimeCorrelationContext
      * @return A string with details about the changed SCLK kernel(s) ready to be logged.
@@ -58,7 +55,6 @@ public class SclkKernelProductDefinition extends EntireFileOutputProductDefiniti
     @Override
     public String getDryRunPrintout(TimeCorrelationContext ctx) throws MmtcException {
         SclkKernel.writeNewProduct(ctx);
-        // ctx.newSclkKernel.get().updateFile();
 
         String[] newSclkEntries = ctx.newSclkKernel.get().getLastXRecords(2);
         // If an interpolated clock change rate has replaced the rate in the existing SCLK kernel record, retrieve the two latest records.
