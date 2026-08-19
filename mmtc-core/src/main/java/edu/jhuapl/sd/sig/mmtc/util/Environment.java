49e661d9c3c8eb366b6524b23a6f39b0a1996664
John Lage <john.lage@jhuapl.edu>
2026-08-19 15:25:13 -0400

    68 adjust CLI exec invocation to satisfy SA checks

--- mmtc-core/src/main/java/edu/jhuapl/sd/sig/mmtc/util/Environment.java
+++ mmtc-core/src/main/java/edu/jhuapl/sd/sig/mmtc/util/Environment.java
@@ -11,7 +11,7 @@ public class Environment {
 	public static boolean isPidRunning(String pid) {
 		try {
 			Process process = Runtime.getRuntime().exec(
-					new String[]{"kill", "-0", pid} // kill -0 returns exit code 0 if process exists, doesn't send any signals
+					new String[]{"/usr/bin/kill", "-0", pid} // kill -0 returns exit code 0 if process exists, doesn't send any signals
 			);
 			return process.waitFor() == 0;
 		} catch (IOException | InterruptedException e) {
